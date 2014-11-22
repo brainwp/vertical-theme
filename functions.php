1e66d822f10903fac827dd943db1fdd07ac8390d
Your Name <diego@brasa.art.br>
2014-11-22 15:54:21 -0200

    subindo tema que estava em producao

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php 
+/* Redefine the header image width and height ********************************************/
+define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyten_header_image_width', 1000 ) );
+define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyten_header_image_height', 345 ) );
+
+// Remove a barra de atualização do Admin
+add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
+
+?>
